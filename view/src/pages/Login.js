86e561658012d1e809fdb9e761d4d03329c79484
Vectormike <victorjonah199@gmail.com>
2019-09-16 13:17:32 +0100

    Func not finished(awaiting url)

--- view/src/pages/Login.js
+++ view/src/pages/Login.js
@@ -1,29 +1,88 @@
-import React from 'react'
-import './Login.css'
-import groupImg from '../assets/23851.png'
+import React, { Component } from "react";
+import "./Login.css";
+import groupImg from "../assets/23851.png";
 
-const Login = () =>{
-	return(
-		<div className="Login">
-			<div className="form-con">
-			<form action="">
-				<h1 className="text-primary">Login</h1>
-				<label htmlFor="username" className="label-brk">Email</label>
-				<input type="email" name="username" id="username"/>
-				<label htmlFor="password" className="label-brk">Password</label>
-				<input type="password" name="password" id="password"/>
-				
-				<label htmlFor="remember" className="label-brk"><input type="checkbox" name="remember" id="remember"/>Remember Me</label>
-				<button className="btn-primary">Login</button>
-				<button className="btn-transparent-default">Forgot Password</button>
-				<button className="btn-transparent-primary">Don't have an account?</button>
-			</form>
-			</div>
-			<div className="img-con">
-				<img src={groupImg} alt=""/>
-			</div>
-		</div>
-	)
+class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  onEmailChange = event => {
+    this.setState({
+      email: event.target.value
+    });
+  };
+
+  onPasswordChange = event => {
+    this.setState({
+      email: event.target.value
+    });
+  };
+
+  // Submit
+  onSubmit = event => {
+    const { email, password } = event.target;
+    fetch("", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: email,
+        password: password
+      })
+    })
+      .then(res => res.json())
+      .then(console.log);
+  };
+
+  render() {
+    return (
+      <div className="Login">
+        <div className="form-con">
+          <form onSubmit={this.onSubmit}>
+            <h1 className="text-primary">Login</h1>
+            <label htmlFor="username" className="label-brk">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              onChange={this.onEmailChange}
+              id="email"
+              required
+            />
+            <label htmlFor="password" className="label-brk">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              onChange={this.onPassword}
+              id="password"
+              required
+            />
+
+            <label htmlFor="remember" className="label-brk">
+              <input type="checkbox" name="remember" id="remember" />
+              Remember Me
+            </label>
+            <button className="btn-primary">Login</button>
+            <button className="btn-transparent-default">Forgot Password</button>
+            <button className="btn-transparent-primary">
+              Don't have an account?
+            </button>
+          </form>
+        </div>
+        <div className="img-con">
+          <img src={groupImg} alt="" />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Login
+export default Login;
