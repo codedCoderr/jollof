b1f11135832ac9a8ea28dab13f81a106e0e36f0d
Winning <winninggreat@gmail.com>
2019-09-17 09:07:41 +0100

    Modified login php to only accept raw json body data

--- api/auth/login.php
+++ api/auth/login.php
@@ -7,14 +7,24 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 $response = [];
-if( isset($_POST['email']) && isset($_POST['password'] ) )
-{
-   $email       = $_POST['email'];
-   $password    = $_POST['password'];
+$entityBody = file_get_contents('php://input'); // receives raw input from the request
+$user_found = false;
+
+//    $email       = $_POST['email'];
+//    $password    = $_POST['password'];
+   
+  
+  
    $data        = file_get_contents(__DIR__.'/../../auth.json'); 
    $data        = json_decode($data,true);
-
-
+    try {
+    $entityBody_json = json_decode($entityBody,true); //decodes the raw response and converts it to json so we can access it with keys
+    
+    $email = $entityBody_json['email']; //assign to email
+    $password = $entityBody_json['password']; //assign to password
+    if ($email == "" or $password == ""){
+        
+    } else {
     foreach($data['users'] as $user)
     {
         $userEmail  = $user['email'];
@@ -29,20 +39,26 @@ if( isset($_POST['email']) && isset($_POST['password'] ) )
                 'avatar'    => $user['avatar'],
                 'status'    => $user['status'],
             ];
+
+            $response_json = json_encode(['authenticated'=>true,'user' => $response],true);
+            $user_found = true;
+
+
+            break;  //breaks out of the loop as soon as there is a match
             
         }
     }
+}
+    } catch(exception $e){
+    }
 
-    if(count($response) === 0){
-        $message = "Email or Password incorrect, try again!";
+    if(!$user_found){
+        $message = "Input fields cannot be empty!";
         header('HTTP/1.1 401 Unauthorized');
-        echo json_encode(['status' => 401,'message' => $message]);
+        echo json_encode(['authenticated'=>false,'status' => 401,'message' => $message]);
     }else{
-        foreach ($response as $value) {
-            $data = json_encode($value);
-            echo $data;
-        }
+        echo $response_json;
     }
         
-}
+
 
