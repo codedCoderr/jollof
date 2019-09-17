6eea4fae1b1f030ad85a073778f49955e087313a
Vectormike <victorjonah199@gmail.com>
2019-09-17 10:19:17 +0100

    Save user from login details(func) enabled

--- view/src/App.js
+++ view/src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import './App.css';
-import Login from './pages/Login'
-function App() {
-  return (
-    <div className="App">
-      <Login/>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import Login from "./pages/Login";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      user: {
+        username: "",
+        avatar: "",
+        status: ""
+      }
+    };
+  }
+
+  // Func to save users details to state
+  saveUser = data => {
+    this.setState({
+      user: {
+        username: data.username,
+        avatar: data.avatar,
+        status: data.status
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Login saveUser={this.saveUser} />
+      </div>
+    );
+  }
 }
 
 export default App;
