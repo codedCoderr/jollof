97d06a9e9780c98e372eb2864482d021b79ce5b5
Vectormike <victorjonah199@gmail.com>
2019-09-17 10:32:50 +0100

    Nothing changed

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const http = require('http');
-const url = require('url');
+const http = require("http");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
-  const auth = require('./route/auth');
+  const auth = require("./route/auth");
   const reqUrl = url.parse(req.url, true);
 
-  if (reqUrl.pathname == '/api/auth/login' && req.method === 'POST') {
+  if (reqUrl.pathname == "/api/auth/login" && req.method === "POST") {
     auth.login(req, res);
   } else {
-    res.end('Invalid Endpoint');
+    res.end("Invalid Endpoint");
   }
 });
 const PORT = 5000;
