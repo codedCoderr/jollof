737f10943df77082dabde1b5b5ccbf6f7058342a
Winning <winninggreat@gmail.com>
2019-09-18 23:57:34 +0100

    removed unused code

--- alternate_servers/python_server/server_database.py
+++ alternate_servers/python_server/server_database.py
@@ -1,5 +1,5 @@
 import urllib.parse as up
-from sqlalchemy import create_engine, MetaData, Column, Integer, String, text, exc
+from sqlalchemy import create_engine, Column, Integer, String, text, exc
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import NullPool
