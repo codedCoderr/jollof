77beea13540840db47960f2821f12ff9d3efda40
Vectormike <victorjonah199@gmail.com>
2019-09-18 12:58:57 +0100

    Added Navigation

---
+++ view/src/pages/Navigation.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Navigation({ isSignedIn, onPageChange }) {
+  if (isSignedIn) {
+    return (
+      <nav>
+        <p onClick={() => onPageChange("signout")}>Sign Out</p>
+      </nav>
+    );
+  } else {
+    return (
+      <nav style={{ alignContent: "center" }}>
+        <button onClick={() => onPageChange("fail")}>Sign In</button>
+        <button onClick={() => onPageChange("register")}>Register</button>
+      </nav>
+    );
+  }
+}
+
+export default Navigation;
