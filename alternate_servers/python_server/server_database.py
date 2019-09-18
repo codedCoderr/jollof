c0273ccc37ec6b092c5fb1b0736b224992f4adb9
Winning <winninggreat@gmail.com>
2019-09-18 19:47:32 +0100

    Implemented login via database check

--- alternate_servers/python_server/server_database.py
+++ alternate_servers/python_server/server_database.py
@@ -1,53 +1,79 @@
 import psycopg2
 import urllib.parse as up
-import crypt
+
+from psycopg2 import errors
+from sqlalchemy import create_engine, MetaData, Column, Integer, String, text, exc
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
+from sqlalchemy.sql import expression
 
 DATABASE_URL = "postgres://cochltcv:w90CheA-A1dPIMNYqtqZ56b0xUEi3Df4@salt.db.elephantsql.com:5432/cochltcv"
 up.uses_netloc.append("postgres")
 url = up.urlparse(DATABASE_URL)
 
+engine = create_engine(DATABASE_URL)
+Base = declarative_base(engine)
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String)
+    password = Column(String)
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+
+def loadSession():
+    metadata = Base.metadata
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
 
 
 class ServerDatabase():
 
-
     def __init__(self):
-        self.connection = psycopg2.connect(database=url.path[1:],
-                                user=url.username,
-                                password=url.password,
-                                host=url.hostname,
-                                port=url.port
-                                )
+        self.session = loadSession()
+        self.connection = engine.connect()
 
-        self.cursor = self.connection.cursor()
+    def login_user(self, user: User):
+        print("running log in")
+        select_user = text("select * from users where email = :email and password = crypt(:password1,password)"
+                           )
+        registered_user = self.connection.execute(select_user, email=user.email, password1=user.password).fetchone()
+        if registered_user is None:
+            print("user not registered")
+            return (False,None)
+        print("user registered")
+        return (True,registered_user)
 
+    def register_user(self, user: User):
+        user_exist = False
 
-    def save_to_users(self, data_to_insert:tuple):
+        for registered_user in self.list_all_user():
+            if user.email == registered_user.email:
+                user_exist = True
 
+                break
+        if user_exist:
+            print("User already exist")
+        else:
+            add_user = text("insert into users(email,password) values(:email,crypt(:password ,gen_salt(:bf)))"
+                            )
 
-        postgres_insert_query = """ INSERT INTO users (username, fullname, email , avatar, status, password) VALUES (%s,%s,%s,%s,%s,%s)"""
+            self.connection.execute(add_user, email=user.email, password=user.password, bf="bf")
 
-        self.cursor.execute(postgres_insert_query, data_to_insert)
+    def list_all_user(self):
+        res = self.session.query(User).all()
 
-        self.connection.commit()
+        return res
 
-
-    def check_if_user_exist(self,email) -> bool:
-        postgres_select_query = "select * from users"
-
-        self.cursor.execute(postgres_select_query)
-        all_users = self.cursor.fetchall()
-        print(all_users)
-        for user_row in all_users:
-            print(user_row)
-            if user_row[3] == email:
-                print(email)
-                return True
-        print("new user")
-        return False
-
-
-
-base = ServerDatabase()
-base.check_if_user_exist("codedcoder@gmail.com")
-base.save_to_users(("win","win@gmail.com","winimage","modile dev","winnn"))
+    def close(self):
+        global engine
+        self.session.close()
+        engine= create_engine(DATABASE_URL,poolclass = NullPool)
