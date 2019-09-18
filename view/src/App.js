263c85fc66c2860e3d66f8409a2ca84bb8b2d315
Vectormike <victorjonah199@gmail.com>
2019-09-18 11:19:48 +0100

    ...

--- view/src/App.js
+++ view/src/App.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import './App.css';
-import Login from './pages/Login'
-function App() {
-  return (
-    <div className="App">
-      <Login/>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import Welcome from "./pages/Welcome";
+import Login from "./pages/Login";
+
+class App extends Component {
+  component() {
+    super();
+
+    this.state = {
+      route: "",
+      user: {
+        email: "",
+        password: ""
+      }
+    };
+  }
+
+  // Func to save user
+  saveUser = data => {
+    this.setState({
+      user: {
+        email: data.email,
+        password: data.password
+      }
+    });
+  };
+
+  onPageChange = route => {
+    if (route === "success") {
+      this.setState({ route: "success" });
+    } else if (route === "fail") {
+      this.setState({ route: "fail" });
+    }
+  };
+
+  render() {
+    const { route } = this.state;
+    if (route === "success") {
+      return <Welcome />;
+    } else if (route === "fail") {
+      return <Register />;
+    }
+    return (
+      <div className="App">
+        <Login saveUser={this.saveUser} onPageChange={this.onPageChange} />
+      </div>
+    );
+  }
 }
 
 export default App;
